80bad8c1c8d555bd7a2df63e4852422265c4e228
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-17 16:49:18 -0300

    chore: initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+local map = vim.keymap.set
+
+map("n", "<leader>rr", ":source %<CR>", { desc = "Reload configuration" })
+
+map("i", "jk", "<ESC>", { desc = "exit insert mode" })
+map("n", "<leader>w", ":w<CR>", { desc = "save file" })
+map("n", "<leader>q", ":q!<CR>", { desc = "force quit" })
+map("n", "<leader>e", ":wq<CR>", { desc = "save and exit" })
+
+-- BAR BAR
+map("n", "<C-j>", ":BufferPrevious<CR>")
+map("n", "<C-k>", ":BufferNext<CR>")
+map("n", "<A-e>", ":BufferClose<CR>")
+
+-- NEO TREE
+map("n", "<A-1>", ":Neotree focus<CR>")
+map("n", "<A-q>", ":Neotree close<CR>")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = function()
+    return {
+
+      { "<leader>/", false },
+
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    }
+  end,
+}
