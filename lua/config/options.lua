97d07160d837098764805a7376edeebf5538ddd7
joalvarez <alvarez.joaquinez@gmail.com>
2024-04-03 08:13:16 -0300

    chore: add new configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,7 +29,10 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
-vim.opt.spelllang = "es-ES.utf-8"
+vim.o.undodir = "~/.config/nvim/undodir"
+
+vim.opt.spelllang = "es.utf-8"
+vim.opt.spell = true
 
 vim.o.wrap = true
 
